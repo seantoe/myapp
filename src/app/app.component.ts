8f5beeeffbce74264f7cf695f8a00e2058820cac
Sean Alvarez <monkee.shan@gmail.com>
2024-06-03 22:30:28 +0800

    gitkeep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,15 @@ import { HomeComponent } from './home/home.component';
   standalone: true,
   imports: [HomeComponent],
   template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
